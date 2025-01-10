f97296b1539a8b7d03f33dc2cdd6b73143dd170a
Shruti <dev.shruti27@gmail.com>
2025-01-10 16:17:40 +0100

    add and remove margin when clicking the dropdown btn

--- main.js
+++ main.js
@@ -6,30 +6,41 @@ navToggle.addEventListener("click", () => {
 });
 
 
-document.addEventListener("click", e => {
+
+
+document.addEventListener("click", (e) => {
+  const marginDiv = document.getElementById("margin");
 
   const isDropdownButton = e.target.matches("[data-dropdown-button]");
   if (!isDropdownButton && e.target.closest("[data-dropdown]") != null) return;
 
-  let currentDropdown 
+  let currentDropdown;
   if (isDropdownButton) {
     currentDropdown = e.target.closest("[data-dropdown]");
     currentDropdown.classList.toggle("active");
+
+    if (currentDropdown.classList.contains("active")) {
+      marginDiv.style.marginTop = "3.5em";
+      marginDiv.style.marginBottom = "1.5em";
+    } else {
+      marginDiv.style.marginTop = "0";
+      marginDiv.style.marginBottom = "0";
+    }
   }
 
-document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
+  document.querySelectorAll("[data-dropdown].active").forEach((dropdown) => {
     if (dropdown === currentDropdown) return;
     dropdown.classList.remove("active");
   });
+});
 
-})
+
 
 
 const scrollers = document.querySelectorAll(".scroller");
 if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
   addAnimation();
 }
-
 function addAnimation() {
   scrollers.forEach((scroller) => {
     scroller.setAttribute("data-animated", true);
