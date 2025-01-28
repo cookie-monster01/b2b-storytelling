0d32ee375517d4fc0259bbd4e269be5ba553506e
Shruti <dev.shruti27@gmail.com>
2025-01-28 14:14:38 +0100

    script for auto typing

--- auto-type.js
+++ auto-type.js
@@ -1,1 +1,34 @@
-const services = ["Brand Building", "Differentiation", "Customer Experience", "Customer Acquisition", "Sales Growth", "New Market Entry", "New Product Launch"];
+const dynamicText = document.querySelector(".auto-type");
+const services = [
+  "Brand Building",
+  "Differentiation",
+  "Customer Experience",
+  "Customer Acquisition",
+  "Sales Growth",
+  "New Market Entry",
+  "New Product Launch",
+];
+
+let servicesWordIndex = 0;
+let wordCharIndex = 1;
+let isDeleting = false;
+
+const typeEffect = () => {
+  const currentWord = services[servicesWordIndex];
+  const currentChar = currentWord.substring(0, wordCharIndex);
+  dynamicText.textContent = currentChar;
+
+  if (!isDeleting && wordCharIndex < currentWord.length) {
+    wordCharIndex++;
+    setTimeout(typeEffect, 200);
+  } else if (isDeleting && wordCharIndex > 0) {
+    wordCharIndex--;
+    setTimeout(typeEffect, 100);
+  }else {
+    isDeleting = !isDeleting;
+    servicesWordIndex = !isDeleting ? (servicesWordIndex + 1) % services.length : servicesWordIndex;
+    setTimeout(typeEffect, 1200);
+  }
+};
+
+typeEffect();
